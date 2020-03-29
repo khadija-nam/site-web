b0944b18c81766ad592b7cc00d92475c22555870
AchoujHajar <59529119+AchoujHajar@users.noreply.github.com>
2020-03-29 23:57:46 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,16 @@
  
+<!-- Footer -->
+
+<footer class="w3-container w3-padding-32 w3-light-grey w3-center">
+  
+  <a href="#" class="w3-button w3-black w3-margin"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+  <div class="w3-xlarge w3-section">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+  </div>
+  <p>Designed by: <a title="W3.CSS" target="_blank" class="w3-hover-text-green">khadija nam et hajar achouj</a></p>
+</footer>
